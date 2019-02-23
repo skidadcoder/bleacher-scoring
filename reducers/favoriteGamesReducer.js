232efeaa894f67e62f7239b1ad5c23bf0c8c668d
Bleacher Technologies <bleachertechnologies@Christophers-Mac-mini.local>
2019-02-23 13:24:42 -0500

    Removed console log from a reducer

--- reducers/favoriteGamesReducer.js
+++ reducers/favoriteGamesReducer.js
@@ -8,10 +8,8 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FAVORITE_GAMES_FETCH_START:
-      console.log("FAVORITE_GAMES_FETCH_START")
       return { ...state, data: {}, favoriteGamesFetchStarted: true };
     case FAVORITE_GAMES_FETCH_SUCCESS:
-      console.log("FAVORITE_GAMES_FETCH_SUCCESS")      
       return { ...state, data: { ...state.data, ...action.payload }, favoriteGamesFetchStarted: false };
     default:
       return state;
